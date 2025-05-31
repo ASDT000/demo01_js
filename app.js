19f85ad062daaff88cc0e04b2b29e9a5eaaba5df
ASDT000 <asdinushkatharidu@gmail.com>
2025-05-31 16:14:43 +0530

    objects part 1

--- app.js
+++ app.js
@@ -1,11 +1,54 @@
-const customer = {
-    name : "madhu",
-    age : 20,
-    address : "Kandy",
-    email: "madhu@gmail.com"
-}
+const customer = [];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+console.log(numbers)
+console.log(numbers[2])
+console.log(numbers[9])
 console.log(customer)
-console.log(customer.name)
-console.log(customer.age)
-console.log(customer.address)
-console.log(customer.email)
+
+
+console.log("--------------------")
+console.log("")
+for (let i = 0; i < 10; i++) {
+    console.log(numbers[i])
+}
+console.log()
+console.log("--------------------")
+
+
+console.log()
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+}
+console.log()
+console.log("--------------------")
+console.log()
+console.log()
+// object array :
+const customers = [
+    {
+        name: "madhu",
+        age : 20,
+        email: "madhu@gmail.com"
+    },
+    {
+        name: "thamani",
+        age : 15,
+        email: "thamani@gmail.com"
+    },
+    {
+        name: "pamudini",
+        age : 12,
+        email: "pamu@gmail.com"
+    }
+]
+
+console.log(customers[0])
+console.log()
+console.log(customers)
+console.log()
+console.log("--------------------")
+for (let i = 0; i < customers.length; i++) {
+    console.log(customers[i])
+}
+console.log()
+console.log("--------------------")
