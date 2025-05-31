f3bf501211d725f664530401f2ed4a9c254829c1
ASDT000 <asdinushkatharidu@gmail.com>
2025-05-31 15:46:03 +0530

    variables and function intro

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Hi Dinu");
+{
+    let names = "madhu" // normal variables
+    var age = 23; // can access out of scope
+    const email = "asdt@gmail.com" // can not change value like final
+}
+function sayHello() {
+    let name = "dinu"
+
+    console.log("Hello " + name)
+}
+
+console.log(age)
